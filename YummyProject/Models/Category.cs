4abe2176acfcbf222e1a4256dbe7c55d6af7c742
can gündoğdu <gundogdu.can@hotmail.com>
2024-12-12 22:29:14 +0300

    Add project files.

---
+++ YummyProject/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YummyProject.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+
+
+        public virtual List<Product> Products { get; set; } //navigation property
+
+
+    }
+}
---
+++ YummyProject/Models/ContactInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace YummyProject.Models
+{
+    public class ContactInfo
+    {
+        [Key]
+        public int ContantInfoId { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string MapUrl { get; set; }
+        public string OpenHours { get; set; }
+
+
+    }
+}
