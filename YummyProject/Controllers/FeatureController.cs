d8cc07f9d6abd36e9c2bbb7cd8c54b035f9a0046
can gündoğdu <gundogdu.can@hotmail.com>
2024-12-17 22:46:21 +0300

    17.12.2024 #lesson2

---
+++ YummyProject/Controllers/FeatureController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YummyProject.Context;
+using YummyProject.Models;
+
+namespace YummyProject.Controllers
+{
+    public class FeatureController : Controller
+    {
+        // GET: Feature
+        YummyContext context=new YummyContext();
+        public ActionResult Index()
+        {
+            var values= context.Features.ToList();
+
+            return View(values);
+        }
+
+
+
+        public ActionResult CreateFeature()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateFeature(Feature feature)
+        {
+
+            if (!ModelState.IsValid) 
+            {
+                return View(feature);
+            }
+
+            context.Features.Add(feature);
+            int result=context.SaveChanges();
+            if (result == 0) 
+            {
+                ViewBag.Error = "Değerler kaydedilirken bir hata ile karşılaşıldı.";
+                return View(feature);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteFeature(int id)
+        {
+            var value = context.Features.Find(id);
+            context.Features.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- YummyProject/Models/Feature.cs
+++ YummyProject/Models/Feature.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,22 @@ namespace YummyProject.Models
     public class Feature
     {
         public int FeatureId { get; set; }
+
+        [Required(ErrorMessage ="Resim Url Boş Bırakılamaz!")]
         public string ImageUrl { get; set; }
+
+
+        [Required(ErrorMessage = "Başlık Boş Bırakılamaz!")]
+        [MinLength(5,ErrorMessage ="Başlık, En az 5 karakter içermelidir.")]
         public string Title { get; set; }
+
+
+        [Required(ErrorMessage = "Açıklama Boş Bırakılamaz!")]
+        [MaxLength(100,ErrorMessage ="Açıklama, Maksimum 100 karakter olmalıdır.")]
         public string Description { get; set; }
+
+
+        [Required(ErrorMessage = "Video Url Boş Bırakılamaz!")]
         public string VideoUrl { get; set; }
     }
 }
